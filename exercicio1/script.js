da1a2759ba550c4ab562d4267b9c70616ccf58ad
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-09-08 16:05:06 -0300

    Exercicios turma Ammal, aula 13, dia 08/09

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,26 @@
-const num = //seu numero aqui
+//PARTE 1
+let numero = prompt("Digite um numero.")
+
+    if(numero %2==0)
+console.log("Divisivel por 2")
+    if(numero %3==0){
+console.log("Divisivel por 3.")
+    }
+    if(numero %2==0 && numero%3==0){
+console.log("Divisivel por 2 e 3.")
+    }
+    else{
+        console.log("divisivel por nenhum dos dois")
+    }
+
+//PARTE 2
+
+    if(numero %2 === 0 || numero %3 === 0){
+        console.log("divisivel por 2 OU 3")
+    }
+    else{
+        console.log("não é divisivel por 2 OU 3")
+    }
+    
+
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,9 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+let nome = prompt("Digite seu nome:")
+	nome === "Jose" ? console.log("Oi,Zé"): console.log("Voce nao é o Ze");
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+	const idade=18
+	idade >=18 ? console.log("Pode tirar CNH") : console.log("Não pode tirar a CNH")
+
+	
